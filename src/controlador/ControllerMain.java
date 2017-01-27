b92237fe15839c1524ffaa7dd39ddf1794ee5fa9
mreina <ma.reina.zarate@gmail.com>
2017-01-27 16:47:59 -0600

    Menu

--- src/controlador/ControllerMain.java
+++ src/controlador/ControllerMain.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class ControllerMain extends Application {
@@ -15,6 +16,7 @@ public class ControllerMain extends Application {
 		Scene escena = new Scene(root);
 		primaryStage.setScene(escena);
 		primaryStage.setTitle("Libreria Cheshire");
+		primaryStage.getIcons().add(new Image("/vista/iconos/logo.png"));
 		primaryStage.show();
 	}
 	
